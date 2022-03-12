b7df30e70e7bedf14939f478c003512f7c2c82b9
cobaltgit <criterion@chitco.co.uk>
2022-03-12 15:30:06 +0000

    Custom OpenAPI schema

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,26 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/assets", StaticFiles(directory="app/assets"), name="assets")
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Cobalt API",
+        version="1.0.0a",
+        description="A general purpose API with many endpoints - IN DEVELOPMENT",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {"url": "/assets/cobaltapi_logo.svg"}
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 with open(
     "app/files/facts.txt", "r"
