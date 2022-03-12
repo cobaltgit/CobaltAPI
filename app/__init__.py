9b341467f3dec7d6d763a947e47bc135c4a3507f
cobaltgit <criterion@chitco.co.uk>
2022-03-12 13:21:33 +0000

    Add some routes! Now with random facts!

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+with open(
+    "app/files/facts.txt", "r"
+) as factfile:  # facts list from https://github.com/assaf/dailyhi/blob/master/facts.txt
+    app.facts = [fact.strip() for fact in factfile.readlines()]
+
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,3 +1,20 @@
+from random import sample
+
+from fastapi import HTTPException
+from fastapi.responses import ORJSONResponse, RedirectResponse
+
 from app import app
 
-# views coming soon...
+
+@app.get("/")
+async def redirect_to_docs():
+    """Redirect to API documentation"""
+    return RedirectResponse(url="/docs", status_code=303)
+
+
+@app.get("/facts", response_class=ORJSONResponse)
+async def get_fact(count: int = 1) -> dict[str, str | list[str]]:
+    """Get random facts from a list of 3,090 facts"""
+    if count > len(app.facts):
+        raise HTTPException(status_code=400, detail=f"Attempted to request more than {len(app.facts)} facts")
+    return {"response": sample(app.facts, count)}
