0707258b9a58d2946658a7bccd278805cd38cc67
cobaltgit <criterion@chitco.co.uk>
2022-03-12 19:22:20 +0000

    Handle required info attributes not being set in pyproject.toml

--- run.py
+++ run.py
@@ -3,8 +3,13 @@ from os import cpu_count
 
 from uvicorn import run
 
+from app import info
+
 
 def main():
+    if not all(map(info.get, ("name", "description", "version"))):
+        raise RuntimeError("One of ('name', 'description', 'version') is not set in pyproject.toml")
+
     with open("config.json", "r") as cfg:
         config = load(cfg)
     run(**config, workers=cpu_count() * 2 + 1)
