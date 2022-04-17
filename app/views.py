6c8ba8126b42c3a1b93d3ca0f0c732444a16f5be
cobaltgit <criterion@chitco.co.uk>
2022-04-17 12:43:02 +0100

    Remove random integer endpoint (haha randint)

--- app/views.py
+++ app/views.py
@@ -1,8 +1,7 @@
 import asyncio
 from functools import partial
 from io import BytesIO
-from random import randint, sample
-from sys import maxsize
+from random import sample
 
 import aiohttp
 from fastapi import HTTPException
@@ -30,33 +29,6 @@ async def get_fact(count: int = 1):
         raise HTTPException(status_code=400, detail=f"Count must be at least 1, got {count}")
     samp = sample(app.facts, count)
     return {"response": samp[0] if count == 1 else samp}
-
-
-@app.get("/random/numbers", response_class=ORJSONResponse, tags=["Random"])
-async def random_int(floor: int = 0, ceil: int = 25, count: int = 1):
-    """
-    Get \<count\> random integers between \<floor\> and \<ceil\>  
-    `count`: integer - optional parameter - the number of integers to retrieve - can be anywhere between 1 and 1000 (default: 1)  
-    `floor`: integer - optional parameter - the minimum integer in the range - must be at least `-sys.maxsize`\* (default: 0)  
-    `ceil`: integer - optional parameter - the maximum integer in the range - can be anywhere between `-sys.maxsize + 1`\* and `sys.maxsize`\*, must be greater than `floor` (default: 25)  
-
-    \* `sys.maxsize` is 9223372036854775807 on a 64-bit system - the maximum value of a 64-bit signed integer, or on a 32-bit system, 2147483647
-    """
-    if floor < -maxsize:
-        raise HTTPException(status_code=400, detail=f"Floor integer must be more than {-maxsize}")
-    elif -maxsize + 1 > ceil or ceil > maxsize:
-        raise HTTPException(status_code=400, detail=f"Ceiling integer must be between {-maxsize+1} and {maxsize}")
-    elif count > 1000:
-        raise HTTPException(
-            status_code=400,
-            detail="Cannot generate more than 100,000 random integers",
-        )
-    elif floor > ceil:
-        raise HTTPException(status_code=400, detail="Floor integer must be less than ceiling integer")
-    elif count < 1:
-        raise HTTPException(status_code=400, detail=f"Count must be at least 1, got {count}")
-
-    return {"response": randint(floor, ceil) if count == 1 else [randint(floor, ceil) for _ in range(count)]}
 
 
 @app.get("/fun/memegen", tags=["Fun"])
