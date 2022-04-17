ee3ebbed107502f37601eddf8c5b56cb686dda3a
cobaltgit <criterion@chitco.co.uk>
2022-04-17 12:52:16 +0100

    Use context manager for image

--- app/functions.py
+++ app/functions.py
@@ -18,28 +18,16 @@ def gen_image_macro(image_bytes: bytes | str, top_text: str, bottom_text: str = 
     top_text = top_text.upper()
     bottom_text = bottom_text.upper() if bottom_text else None
 
-    image = Image.open(image_bytes)
-    draw = ImageDraw.Draw(image)
-    w, h = image.size
-    font = ImageFont.truetype(font=font_path, size=h // 10)
-    cw, ch = font.getsize("A")
-    cpl = w // cw
-    top_lines, bottom_lines = (textwrap.wrap(top_text, width=cpl), textwrap.wrap(bottom_text, width=cpl) if bottom_text else None)
+    with Image.open(image_bytes) as image:
+        draw = ImageDraw.Draw(image)
+        w, h = image.size
+        font = ImageFont.truetype(font=font_path, size=h // 10)
+        cw, ch = font.getsize("A")
+        cpl = w // cw
+        top_lines, bottom_lines = (textwrap.wrap(top_text, width=cpl), textwrap.wrap(bottom_text, width=cpl) if bottom_text else None)
 
-    y = 10
-    for line in top_lines:
-        lw, lh = font.getsize(line)
-        x = (w - lw) / 2
-        draw.text((x - 1, y), line, font=font, fill="black")
-        draw.text((x + 1, y), line, font=font, fill="black")
-        draw.text((x, y - 1), line, font=font, fill="black")
-        draw.text((x, y + 1), line, font=font, fill="black")
-        draw.text((x, y), line, fill="white", font=font)
-        y += lh
-    
-    if bottom_lines is not None:
-        y = h - ch * len(bottom_lines) - 15
-        for line in bottom_lines:
+        y = 10
+        for line in top_lines:
             lw, lh = font.getsize(line)
             x = (w - lw) / 2
             draw.text((x - 1, y), line, font=font, fill="black")
@@ -48,8 +36,20 @@ def gen_image_macro(image_bytes: bytes | str, top_text: str, bottom_text: str = 
             draw.text((x, y + 1), line, font=font, fill="black")
             draw.text((x, y), line, fill="white", font=font)
             y += lh
+        
+        if bottom_lines is not None:
+            y = h - ch * len(bottom_lines) - 15
+            for line in bottom_lines:
+                lw, lh = font.getsize(line)
+                x = (w - lw) / 2
+                draw.text((x - 1, y), line, font=font, fill="black")
+                draw.text((x + 1, y), line, font=font, fill="black")
+                draw.text((x, y - 1), line, font=font, fill="black")
+                draw.text((x, y + 1), line, font=font, fill="black")
+                draw.text((x, y), line, fill="white", font=font)
+                y += lh
 
-    out = BytesIO()
-    image.save(out, format=image.format.lower())
+        out = BytesIO()
+        image.save(out, format=image.format.lower())
     out.seek(0)
     return out
